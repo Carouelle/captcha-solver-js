b332ef7972e30aa40bd6ec636597d855c01da092
Carl Ouellette <carlouellette2@gmail.com>
2017-11-14 23:56:33 -0500

    WIP download image data

--- captcha_solver.js
+++ captcha_solver.js
@@ -1,21 +1,9 @@
-function getCaptchaImage() {
-    var imageNode = submitForm.getElementsByTagName('img')[0];
-
-    var rawImage = getRawImage(imageNode);
-    
-}
-
-function writeAnswerInCaptchaInput() {
-    var inputForm = document.getElementsByName('noCaptcha')[0];
-    inputForm.value = 'Hello World'
-}
-
 var submitForm = document.getElementsByName('form1')[0];
 var btnGetImage = document.createElement('button');
 btnGetImage.onclick = getCaptchaImage;
 document.body.appendChild(btnGetImage);
 
-var btnGetImageText = document.createTextNode("Get Base64 image");
+var btnGetImageText = document.createTextNode("Get image");
 btnGetImage.appendChild(btnGetImageText);
 
 var btnWriteAnswer = document.createElement('button');
@@ -25,9 +13,26 @@ document.body.appendChild(btnWriteAnswer);
 var btnWriteAnswerText = document.createTextNode("Write in input");
 btnWriteAnswer.appendChild(btnWriteAnswerText);
 
+function getCaptchaImage() {
+    var imageNode = submitForm.getElementsByTagName('img')[0];
+    var rawImage = getRawImage(imageNode);
+
+    var stringByteArray = "";
+    for (var i = 0; i < rawImage.height * rawImage.width * 4; i++) {
+        stringByteArray += rawImage.data[i] + ',';
+    }
+
+    location.href = 'data:text/plain:charset=utf-8,' + encodeURIComponent(stringByteArray);
+}
+
+function writeAnswerInCaptchaInput() {
+    var inputForm = document.getElementsByName('noCaptcha')[0];
+    inputForm.value = 'Hello World'
+}
+
 /**
- * Gets raw image data from img node
- * @param {Node} img
+ * Gets raw image data from Image element
+ * @param {Image} img
  * @returns {ImageData}
  */
 function getRawImage(img) {
